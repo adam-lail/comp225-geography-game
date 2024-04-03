47b0110480e6e357b167527bec83bd31f8cf7fa7
kendall-sullivan <123098060+kendall-sullivan@users.noreply.github.com>
2024-04-03 15:21:17 -0500

    get 2 random countries function

--- web-app/src/path_algorithm.js
+++ web-app/src/path_algorithm.js
@@ -1,5 +1,9 @@
 import un_borders from './un_borders.js'
 
+import countriesHashMap from './country_codes.js'
+
+/* ----PATH CHECKING FUNCTIONS---- */
+
 // function based on this pseudocode/tutorial: https://yuminlee2.medium.com/breadth-first-search-bfs-algorithm-b93ef5258c4d#:~:text=BFS%20can%20be%20used%20to,level%20neighbors%2C%20and%20so%20on.
 function bfs(country1, country2) {
     var queue = []
@@ -10,7 +14,7 @@ function bfs(country1, country2) {
         var curr = queue[0]
         removeItemOnce(queue, queue[0])
         if (curr == country2) {
-            return "yes"
+            return true
         }
         if (un_borders[curr] == null) {
             continue
@@ -23,10 +27,10 @@ function bfs(country1, country2) {
             })
         }
     }
-    return "no"
+    return false
 }
 
-function bfs_with_user_countries(country1, country2, countries) {
+export function bfs_with_user_countries(country1, country2, countries) {
     var queue = []
     queue.push(country1)
     var visited = {}
@@ -35,7 +39,7 @@ function bfs_with_user_countries(country1, country2, countries) {
         var curr = queue[0]
         removeItemOnce(queue, queue[0])
         if (curr == country2) {
-            return "yes"
+            return true
         }
         if (un_borders[curr] == null) {
             continue
@@ -48,8 +52,35 @@ function bfs_with_user_countries(country1, country2, countries) {
             })
         }
     }
-    return "no"
+    return false
 }
+
+
+/* ----GET END COUNTRIES FUNCTIONS---- */
+
+
+function getRandomCountry() {
+    const dictLength = Object.keys(countriesHashMap).length
+    var randomKey = Object.keys(countriesHashMap)[getRandomInt(0, dictLength)];
+    return countriesHashMap[randomKey]
+}
+
+export function chooseEndCountries() {
+    var country1 = ""
+    var country2 = ""
+    var arr = [country1, country2]
+    while (!(bfs(country1, country2) && (country1 != country2))) {
+        country1 = getRandomCountry()
+        country2 = getRandomCountry()
+        arr = [country1, country2]
+    }
+    return arr
+}
+
+
+
+/* ----HELPER FUNCTIONS---- */
+
 
 // function copied from https://stackoverflow.com/questions/5767325/how-can-i-remove-a-specific-item-from-an-array-in-javascript
 function removeItemOnce(arr, value) {
@@ -60,6 +91,17 @@ function removeItemOnce(arr, value) {
     return arr;
 }
 
+// function copied from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getRandomInt(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+}
+
+
+
+/* ----PRINT STATEMENT TESTS---- */
+
 // console.log(bfs("es", "pl")) //yes
 // console.log(bfs("es", "us")) //no
 // console.log(bfs("ad", "fr")) //yes
@@ -67,9 +109,13 @@ function removeItemOnce(arr, value) {
 // console.log(bfs("au", "id")) //no
 // console.log(bfs("ad", "ad")) //yes
 
-var countries = ["es", "pl", "de", "pt", "ad", "fr"]
-console.log(bfs_with_user_countries("es", "pl", countries))
+// var countries = ["es", "pl", "de", "pt", "ad", "fr"]
+// console.log(bfs_with_user_countries("es", "pl", countries))
+// var countries2 = ["es", "pl", "us"]
+// console.log(bfs_with_user_countries("es", "pl", countries2))
 
-var countries2 = ["es", "pl", "us"]
-console.log(bfs_with_user_countries("es", "pl", countries2))
+// console.log(getRandomCountry())
+
+console.log(chooseEndCountries())
+
 
